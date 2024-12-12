e1003c3c6bb432f0d7951b7cc55d2d9f048d68ab
Goromchik <149601826+Goromchik@users.noreply.github.com>
2024-12-12 17:16:01 +0000

    Add files via upload

--- mainwindow.h
+++ mainwindow.h
@@ -4,6 +4,8 @@
 #include <QMainWindow>
 #include "loginform.h"
 #include "registerform.h"
+#include "mainchatwindow.h"
+#include "chatwindow.h"
 
 class MainWindow : public QMainWindow {
     Q_OBJECT
@@ -11,14 +13,21 @@ class MainWindow : public QMainWindow {
 public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
+    QString url = "http://192.168.0.141:8300/token_login";
 
 private slots:
     void showRegisterForm();
     void showLoginForm();
+    void showMainChatWindow();
+    void showChatWindow(const QString &chatName, const QString &chatId, const QString &recipientId);
 
 private:
+    bool checkTokens();
+    void validateTokens();
     LoginForm *loginForm;
     RegisterForm *registerForm;
+    MainChatWindow *mainChatWindow;
+    ChatWindow *chatWindow;
 };
 
 #endif // MAINWINDOW_H
