1bac780c84fd7c439664e9a354352ed93e01504b
Goromchik <149601826+Goromchik@users.noreply.github.com>
2024-12-12 19:05:59 +0000

    Add files via upload

---
+++ mainchatwindow.h
@@ -0,0 +1,36 @@
+#ifndef MAINCHATWINDOW_H
+#define MAINCHATWINDOW_H
+
+#include <QWidget>
+#include <QLineEdit>
+#include <QListWidget>
+#include <QPushButton>
+#include <QVBoxLayout>
+#include <QHBoxLayout>
+#include <QJsonArray>
+#include "websocketclient.h"
+
+class MainChatWindow : public QWidget {
+    Q_OBJECT
+
+public:
+    explicit MainChatWindow(QWidget *parent = nullptr);
+    QString matching_url = "http://212.34.139.173:8400/matching";
+
+signals:
+    void chatSelected(const QString &chatName, const QString &chatId, const QString &recipientId);
+
+private slots:
+    void onMessageReceived(const QString &message);
+    void fetchChats();
+    void updateChatList(const QJsonArray &chats);
+    void createNewChat();
+
+private:
+    QLineEdit *searchBar;
+    QListWidget *chatList;
+    QPushButton *addChatButton;
+    WebSocketClient *webSocketClient;
+};
+
+#endif // MAINCHATWINDOW_H
